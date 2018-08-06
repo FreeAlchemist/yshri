6a8eb7b258bb0203fa338d0d444f002f442e1eba
FreeAlchemist <freealchemist@mail.ru>
2018-08-06 17:52:06 +0300

    Add files via upload

--- entrance-task-2/js/task2.js
+++ entrance-task-2/js/task2.js
@@ -19,7 +19,8 @@ function deviceMoveRight(){
 		el.style.left = newposition+"px"
 	}
 }
-function deviceFilter(elclass){
+function deviceFilter(elclass,event){
+	var event = event || window.event
 	var menuelements = document.getElementsByClassName('device-menupart')
 	for (var i = menuelements.length - 1; i >= 0; i--) {
 		menuelements[i].setAttribute('class','device-menupart')
@@ -39,8 +40,9 @@ function deviceFilter(elclass){
 		elements[i].style.display = "block"
 	}
 }
-function showTemp(){
+function showTemp(event){
 	// console.log(event.currentTarget.className)
+	var event = event || window.event
 	var el = event.currentTarget
 	var elval = el.value
 	// console.log(elval)
@@ -50,8 +52,9 @@ function showTemp(){
 }
 
 
-function deviceShowPopup(type){
+function deviceShowPopup(type,event){
 	// alert(event.currentTarget.className)
+	var event = event || window.event
 	var currentel = event.currentTarget
 	var elinfo = currentel.getElementsByClassName('switch')
 	var elname = currentel.getElementsByClassName('device-name')
@@ -61,27 +64,54 @@ function deviceShowPopup(type){
 	var status = elstatus[0].innerHTML
 	var currenttemp = '+23'
 
-	var elposition = currentel.getBoundingClientRect();
-	var elX = elposition.left + window.scrollX;
-	var elY = elposition.right + window.scrollY;
-
-
-	// console.log(name)
-	// console.log(status)
-	console.log(currentel.className)
-	console.log(elX)
-	console.log(elY)
-
-
 	var el = document.getElementById('popup')
 	el.style.display="block"
-	console.log(el.style.transform.origin)
-	// el.style.transformorigin = "100% 0%"
+
+	var popupform = document.getElementById('popup-form')
 	var popupname = document.getElementById('popup-name')
 	var popupstatus = document.getElementById('popup-status')
 	var popupinfo = document.getElementById('popup-info')
 	var popupmenu = document.getElementById('popup-menu')
 	var popupslider = document.getElementById('popup-slider')
+
+	var elposition = currentel.getBoundingClientRect();
+	var elX = elposition.left + window.scrollX;
+	var elY = elposition.right + window.scrollY;
+	var anidiff = 367-elX
+	if(anidiff > 0){
+		var anidiffmin = parseInt('-'+anidiff)
+	}
+	else{
+		var anidiffmin = Math.abs(parseInt(anidiff))
+	}
+	// console.log(name)
+	// console.log(status)
+	// console.log(currentel.className)
+	// console.log(elX)
+	// console.log(elY)
+	// console.log('anidiff')
+	// console.log(anidiff)
+	// console.log(anidiffmin)
+
+	var animation = popupform.animate([
+			// {transform: 'translate(-327px,530px) scale(0)'},
+			{transform: 'translate('+anidiffmin+'px,530px) scale(0)'},
+			{transform: 'translate(0px,0px) scale(1)'}
+		],400)
+	//Animate popup
+	/*
+		 transform: translate(0px,530px) scale(0);
+		transform: translate(-327px,530px) scale(0);
+		transform: translate(-112px,530px) scale(0);
+		transform: translate(-112px,650px) scale(0);
+		transform: translate(103px,530px) scale(0);
+		transform: translate(318px,530px) scale(0);
+		transform: translate(533px,530px) scale(0);
+		transform: translate(748px,160px) scale(0);
+
+
+		transform: translate(0px,0px) scale(1);
+	*/
 
 	popupname.innerHTML=""
 	popupstatus.innerHTML=""
@@ -106,7 +136,7 @@ function deviceShowPopup(type){
 		if(currenttemp){popupinfo.innerHTML= "<div id='popup-info-temp'>"+currenttemp+"</div><div class='"+info+"'/>"}
 		popupmenu.innerHTML="<div class='device-menupart active'>Вручную</div><div class='device-menupart'>Холодно</div><div class='device-menupart'>Тепло</div><div class='device-menupart'>Жарко</div>"
 		// popupslider.innerHTML="<div class='mintemp'>-10</div><div class='maxtemp'>30</div><input type='range' class='slider slider-temp' min='-10' max='30' value='23'/>"
-		popupslider.innerHTML="<input type='range' class='slider slider-temp' min='-10' max='30' value='23' onchange='showTemp()'/>"
+		popupslider.innerHTML="<input type='range' class='slider slider-temp' min='-10' max='30' value='23' step='1' onchange='showTemp(event)'/>"
 	}
 	else{
 		popupslider.innerHTML="Undefined device type"
@@ -114,8 +144,10 @@ function deviceShowPopup(type){
 	if(popupmenu.innerHTML==""){popupmenu.style.display="none"}
 		else{popupmenu.style.display="block"}
 }
-function deviceHidePopup(){
+function deviceHidePopup(event){
 	var el = document.getElementById('popup')
+	// var elform = document.getElementById('popup-form')
+	// elform.setAttribute('class','deviceHide')
 	el.style.display="none"
 }
 function scenarioMoveLeft(elid){
